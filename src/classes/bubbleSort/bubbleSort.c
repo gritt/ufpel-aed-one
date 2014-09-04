ff01e29eb0a545f5c5e79145db70d82f0dc411e3
Gilvan Ritter <gilvanritter@gmail.com>
2014-09-03 23:31:37 -0300

    starting bubble sort implementation and analysis

---
+++ src/classes/bubbleSort/bubbleSort.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int rows, arrayToBeSort[];
+    
+    //read how many rows have to read
+    scanf("%d", &rows);
+    
+    
+    //allocate space for all inputs
+    if (### == NULL) {
+        printf("Impossible to allocate space");
+        exit(0);
+    }
+    
+    arrayToBeSort[] = malloc(rows * sizeof(int))
+    
+    return 0;
+}
+
+
+int bubbleSort()
+{
+
+}
