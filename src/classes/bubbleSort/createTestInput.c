6d896f3f44630afb99990520f9274ba11ceb6b49
Gilvan Ritter <gilvanritter@gmail.com>
2014-09-04 01:16:08 -0300

    added script to create test inputs

--- src/classes/bubbleSort/createTestInput.c
+++ src/classes/bubbleSort/createTestInput.c
@@ -1,7 +1,21 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 int main()
 {
+    int i, values;
     
+    scanf("%d", &values);
+    
+    //how many lines
+    printf("%d \n", values);
+    
+    //stdout > file
+    for(i = 0 ; i < values ; i++) {
+        
+        printf("%d \n", rand());
+    }
 
+    return 0;
 }
