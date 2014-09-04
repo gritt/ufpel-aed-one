82cc15b1aa4707f45befc9b93369671ef85a5220
Gilvan Ritter <gilvanritter@gmail.com>
2014-09-04 00:05:00 -0300

    storing input values on vector

--- src/classes/bubbleSort/bubbleSort.c
+++ src/classes/bubbleSort/bubbleSort.c
@@ -3,25 +3,68 @@
 
 int main()
 {
-    int rows, arrayToBeSort[];
+    int rows, iterator, rowValue, *arrayToBeSort;
     
-    //read how many rows have to read
+    //first line - how many rows have to read
     scanf("%d", &rows);
     
     
     //allocate space for all inputs
-    if (### == NULL) {
+    if ((arrayToBeSort = malloc(rows * sizeof(int))) == NULL) {
         printf("Impossible to allocate space");
         exit(0);
     }
     
-    arrayToBeSort[] = malloc(rows * sizeof(int))
+    arrayToBeSort = malloc(rows * sizeof(int));
+    
+    
+    //store all in vector
+    for (iterator=0; iterator<rows; iterator++) {
+        
+        scanf("%d", &rowValue);
+        
+        arrayToBeSort[iterator] = rowValue;
+        
+        printf("%d\n", rowValue);
+    }
     
     return 0;
 }
 
 
-int bubbleSort()
-{
-
-}
+//int bubbleSort()
+//{
+//    int i, temp,
+//    
+//    bool isChanged = false;
+//    
+//    do {
+// 
+//        for (i=1; n - 1; i++) {
+//            
+//            
+//            if (v[i] > v[i+1]) {
+//                temp = v[i];
+//                v[i] = v[i+1];
+//                v[i+1] = temp;
+//                isChanged = true;
+//            }
+//            
+//        }
+//        
+//        
+//    } while (isChanged == false);
+    
+/*
+ *
+ inicio
+    repita
+        trocou ← falso paraide1at ́en-1 hacer
+        se v[i] > v[i+1] ent ̃ao temp ← v[i]
+            v[i] ← v[i+1]
+            ￼￼￼￼v[i+1] ← temp trocou ← verdadeiro
+        ￼￼ate nao trocou
+ */
+    
+//    return 0;
+//}
