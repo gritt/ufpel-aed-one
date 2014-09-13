65870b18a7f51391bca8b7a45f789587ff6758e2
Gilvan Ritter <gilvanritter@gmail.com>
2014-09-13 20:20:12 -0300

    added part of the logic on movie.c on parser.c

--- src/classes/locadoraDoAlcemar/parser.c
+++ src/classes/locadoraDoAlcemar/parser.c
@@ -1,3 +1,87 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
+#include "movie.h"
+
+void parseCatalog(int rows, FILE *file, movieType *movie)
+{
+    int rowSize;
+    int titleStringSize;
+    int categoryStringSize;
+    char rowString[255];
+
+
+    printf("here");
+    for (int i = 1; i < rows; i++) {
+        
+        //all row contents
+        fgets(rowString, 255, file);
+        
+        //how much chars in this row
+        rowSize = strlen(rowString);
+        
+        //skip invalid rows
+        if (rowSize <= 1) {
+            continue;
+        }
+        
+        movie[i].id = i;
+        
+    
+        int isWritingOn = 0;
+        
+        /*
+         
+        for (int j=0; j < rowSize; j++) {
+            
+            printf("%s\n", rowString);
+            
+            if (rowString[j] == ';') {
+                //change where it's going to save
+                isWritingOn++;
+                
+                //skip the ';'
+                j++;
+            }
+            
+            
+            //save to title
+            if (isWritingOn == 0) {
+                
+                titleStringSize = strlen(movie[i].title);
+                
+                movie[i].title[titleStringSize] = rowString[j];
+                
+                titleStringSize++;
+                break;
+                
+            }
+            
+            //save to year
+            if (isWritingOn == 1) {
+                break;
+            }
+            
+            //save to copies
+            if (isWritingOn == 2) {
+                break;
+            }
+            
+            //save to category
+            if (isWritingOn == 3) {
+                
+                categoryStringSize = strlen(movie[i].category);
+                
+                movie[i].category[categoryStringSize] = rowString[j];
+                
+                categoryStringSize++;
+                break;
+            }
+            
+        }
+        
+        */
+    }
+    
+}
--- src/classes/locadoraDoAlcemar/parser.h
+++ src/classes/locadoraDoAlcemar/parser.h
@@ -1,6 +1,6 @@
-#ifndef MOVIE_H_
-#define MOVIE_H_
+#ifndef PARSER_H_
+#define PARSER_H_
 
-//
+void parseCatalog(int rows, FILE *file, movieType *movie);
 
 #endif
