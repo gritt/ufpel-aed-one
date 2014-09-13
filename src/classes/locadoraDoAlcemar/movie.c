978b35a24c8fc2f10bf5f8d222d33b3a5055dcc3
Gilvan Ritter <gilvanritter@gmail.com>
2014-09-13 17:20:35 -0300

    added logic to read file by rows, then read from row string to strcut

--- src/classes/locadoraDoAlcemar/movie.c
+++ src/classes/locadoraDoAlcemar/movie.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #include "movie.h"
-#include "parser.h"
 
 struct movieMetadata
 {
@@ -10,8 +10,8 @@ struct movieMetadata
     int year;
     int copies;
     
-    char category;
     char title[100];
+    char category[100];
 };
 
 /*
@@ -47,50 +47,98 @@ int canAllocateMemory(int rows)
  */
 int populateMoviesCatalog()
 {
-    int i, rows;
-    char rowString[255];
+    int rows, rowSize, titleStringSize, categoryStringSize;
+    char rowString[255], character;
     
     struct movieMetadata *movie;
     FILE *file;
     
     
     canOpenFile(file); //check
-    file = fopen("input.txt", "r");
+    file = fopen("inputDev.txt", "r");
     
     
     fscanf(file, "%d", &rows);
     
-    
+
     canAllocateMemory(rows); //check
     movie = malloc(rows * sizeof(struct movieMetadata));
     
     
-    
-    
-    
-    
-    //todo idea.
-    for (i = 1; i < rows; i++) {
+    for (int i = 1; i < rows; i++) {
         
         //all row contents
         fgets(rowString, 255, file);
         
+        //how much chars in this row
+        rowSize = strlen(rowString);
 
-        movie[i].id     =   i;
-        movie[i].title  =   getTitle(rowString);
-        movie[i].year   =   getYear(rowString);
-        movie[i].category = getCategory(rowString);
+        //skip invalid rows
+        if (rowSize <= 1) {
+            continue;
+        }
+
+        movie[i].id = i;
+
+        //________0________;__1__;2;_3__
+        //O DOSSIE DE ODESSA;1998;2;DRAMA
+        int isWritingOn = 0;
+        
+        for (int j=0; j < rowSize; j++) {
+            
+            
+            if (rowString[j] == ';') {
+                //change where it's going to save
+                isWritingOn++;
+
+                //skip the ';'
+                j++;
+            }
+            
+            switch (isWritingOn) {
+
+                case 0:
+                    //save to title
+                    titleStringSize = strlen(movie[i].title);
+                    
+                    movie[i].title[titleStringSize] = rowString[j];
+
+                    titleStringSize++;
+                    break;
+                
+                case 1:
+                    //save to year
+//                    movie[i].year = rowString[j];
+//                    printf("%c", rowString[j]);
+                    break;
+                
+                case 2:
+                    //save to copies
+//                    movie[i].copies = rowString[j];
+//                    printf("%c", rowString[j]);
+                    break;
+                
+                case 3:
+                    //save to category
+                    categoryStringSize = strlen(movie[i].category);
+                    
+                    movie[i].category[categoryStringSize] = rowString[j];
+                    
+                    categoryStringSize++;
+                    break;
+
+                default:
+                    //look at my horse, my horse is amazing
+                    break;
+            }
+        }
+
+//        printf("%s", rowString);
     }
     
-    printf("%d", rows);
+//    printf("%d", rows);
     
-    
-    
-    
-    
-    
-    
-    
+
     return 1;
 }
 
