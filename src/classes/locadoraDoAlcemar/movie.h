5da7a8c18404c703d461a1cbed0195db4d686234
Gilvan Ritter <gilvanritter@gmail.com>
2014-09-13 20:20:46 -0300

    moved struct to header to be user while populating data on parser

--- src/classes/locadoraDoAlcemar/movie.h
+++ src/classes/locadoraDoAlcemar/movie.h
@@ -1,6 +1,18 @@
 #ifndef MOVIE_H_
 #define MOVIE_H_
 
+struct movieMetadata
+{
+    int id;
+    int year;
+    int copies;
+    
+    char title[100];
+    char category[100];
+};
+
+typedef struct movieMetadata movieType;
+
 int populateMoviesCatalog();
 
 #endif
