479f6f07e537266602a42b5b349c7f85a58e9ef8
Gilvan Ritter <gilvanritter@gmail.com>
2014-09-14 21:25:25 -0300

    writing data on struct, added counter to write on proper position

--- src/classes/locadoraDoAlcemar/parser.c
+++ src/classes/locadoraDoAlcemar/parser.c
@@ -7,35 +7,45 @@
 void parseCatalog(int rows, FILE *file, movieType *movie)
 {
     int rowSize;
+    char rowString[255];
+    
     int titleStringSize;
     int categoryStringSize;
-    char rowString[255];
 
+    
+    
+    printf ("reading library, it can take some miliseconds\n");
 
-    printf("here");
-    for (int i = 1; i < rows; i++) {
+    
+    
+    for (int i = 0; i < rows; i++) {
+        
+        //pre set vars
+        int isWritingOn = 0;
+        titleStringSize = 0;
+        categoryStringSize = 0;
+        
+        
+        
+        //save id
+        movie[i].id = i;
+        
+        
         
         //all row contents
         fgets(rowString, 255, file);
         
         //how much chars in this row
         rowSize = strlen(rowString);
-        
+
         //skip invalid rows
         if (rowSize <= 1) {
             continue;
         }
         
-        movie[i].id = i;
         
-    
-        int isWritingOn = 0;
         
-        /*
-         
         for (int j=0; j < rowSize; j++) {
-            
-            printf("%s\n", rowString);
             
             if (rowString[j] == ';') {
                 //change where it's going to save
@@ -45,43 +55,23 @@ void parseCatalog(int rows, FILE *file, movieType *movie)
                 j++;
             }
             
-            
-            //save to title
             if (isWritingOn == 0) {
-                
-                titleStringSize = strlen(movie[i].title);
                 
                 movie[i].title[titleStringSize] = rowString[j];
                 
                 titleStringSize++;
-                break;
-                
             }
-            
-            //save to year
-            if (isWritingOn == 1) {
-                break;
-            }
-            
-            //save to copies
-            if (isWritingOn == 2) {
-                break;
-            }
-            
-            //save to category
-            if (isWritingOn == 3) {
-                
-                categoryStringSize = strlen(movie[i].category);
-                
-                movie[i].category[categoryStringSize] = rowString[j];
-                
-                categoryStringSize++;
-                break;
-            }
-            
+    
         }
-        
-        */
     }
     
+
+    
+    //testing, print
+    for (int k=0; k < rows; k++) {
+        
+        printf("%d \n", movie[k].id);
+        printf("%s \n", movie[k].title);
+    }
+
 }
