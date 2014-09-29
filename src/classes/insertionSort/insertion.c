6743dee5907e6a0cfb862e7f8ce63760207db1ab
Gilvan Ritter <gilvanritter@gmail.com>
2014-09-29 01:16:09 -0300

    added base functions and header

--- src/classes/insertionSort/insertion.c
+++ src/classes/insertionSort/insertion.c
@@ -5,7 +5,12 @@ void insertionSort(int *array, int arraySize)
 	
 }
 
-void printArray(int *array, int arraySize)
+void populateTheArray(int *array, int arraySize)
+{
+
+}
+
+void printTheArray(int *array, int arraySize)
 {
     
 }
--- src/classes/insertionSort/insertion.h
+++ src/classes/insertionSort/insertion.h
@@ -2,6 +2,7 @@
 #define INSERTION_H_
 
 void insertionSort(int *array, int arraySize);
-void printArray(int *array, int arraySize);
+void populateTheArray(int *array, int arraySize);
+void printTheArray(int *array, int arraySize);
 
 #endif
