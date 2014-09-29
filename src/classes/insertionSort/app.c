d67bbe94032d12aac93d9ac5a063233ae4908129
Gilvan Ritter <gilvanritter@gmail.com>
2014-09-29 01:16:40 -0300

    call to insertionSort and other functions

--- src/classes/insertionSort/app.c
+++ src/classes/insertionSort/app.c
@@ -1,8 +1,30 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 #include "insertion.h"
 
 int main()
 {
-    printf("hell-o world");
+    int arraySize;
+    int *theArray;
+    
+    scanf("%d", &arraySize);
+    
+    if ((theArray = malloc(arraySize * sizeof(int))) == NULL) {
+        printf("Failed to allocated memory. exit");
+    }
+    
+    theArray = malloc(arraySize * sizeof(int));
+    
+    
+
+    
+    populateTheArray(theArray, arraySize);
+    
+    printTheArray(theArray, arraySize);
+    
+    insertionSort(theArray, arraySize);
+    
+
+    return 1;
 }
