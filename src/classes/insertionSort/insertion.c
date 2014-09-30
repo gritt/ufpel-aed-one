a03581e5514bf5bff41e4e426f7171312655b4c4
Gilvan Ritter <gilvanritter@gmail.com>
2014-09-29 23:04:34 -0300

    develop function to print array in pretty format and populate the array with stdin

--- src/classes/insertionSort/insertion.c
+++ src/classes/insertionSort/insertion.c
@@ -7,10 +7,26 @@ void insertionSort(int *array, int arraySize)
 
 void populateTheArray(int *array, int arraySize)
 {
-
+    for (int i=1; i < arraySize; i++) {
+        
+        scanf("%d", &array[i]);
+    }
 }
 
 void printTheArray(int *array, int arraySize)
 {
+    printf("{");
     
+    for (int i=1; i < arraySize; i++) {
+        printf("%d", array[i]);
+
+        //dont print comma for the last int in array
+        if (i+1 == arraySize) {
+            continue;
+        }
+        
+        printf(",");
+    }
+    
+    printf("}");
 }
