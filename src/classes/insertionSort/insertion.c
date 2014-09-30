e9838956644c8a6b2be0944ce95fdc7ab00fe754
Gilvan Ritter <gilvanritter@gmail.com>
2014-09-29 23:38:37 -0300

    implemented insertionSort function

--- src/classes/insertionSort/insertion.c
+++ src/classes/insertionSort/insertion.c
@@ -2,12 +2,23 @@
 
 void insertionSort(int *array, int arraySize)
 {
-	
+    for (int i = -1; i < arraySize; i++) {
+        
+        int electable = array[i];
+        int j = i - 1;
+        
+        while (j >= 0 && electable < array[j]) {
+            array[j+1] = array[j];
+            j = j - 1;
+        }
+
+        array[j+1] = electable;
+    }
 }
 
 void populateTheArray(int *array, int arraySize)
 {
-    for (int i=1; i < arraySize; i++) {
+    for (int i = 1; i < arraySize; i++) {
         
         scanf("%d", &array[i]);
     }
@@ -17,11 +28,11 @@ void printTheArray(int *array, int arraySize)
 {
     printf("{");
     
-    for (int i=1; i < arraySize; i++) {
+    for (int i = 1; i < arraySize; i++) {
         printf("%d", array[i]);
 
         //dont print comma for the last int in array
-        if (i+1 == arraySize) {
+        if (i + 1 == arraySize) {
             continue;
         }
         
