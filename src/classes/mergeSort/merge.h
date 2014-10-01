b356493f64e49087bbd07057751a519b86830698
Gilvan Ritter <gilvanritter@gmail.com>
2014-10-01 04:41:58 -0300

    mergeSort header

--- src/classes/mergeSort/merge.h
+++ src/classes/mergeSort/merge.h
@@ -1,8 +1,15 @@
 #ifndef MERGE_H_
 #define MERGE_H_
 
+#define ARRAY_SIZE_LIMIT 10
 
-void mergeSort(int *array, int arraySize);
+void mergeSort(int *array, int *arrayCopy, int begin, int end);
+
+void merge(int *array, int *arrayCopy, int begin, int middle, int end);
+
+void copyArray(int *array, int *arrayCopy, int begin, int end);
+
+
 void populateTheArray(int *array, int arraySize);
 void printTheArray(int *array, int arraySize);
 
