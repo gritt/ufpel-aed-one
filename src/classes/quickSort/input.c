2370b917e7d0f7c55204352cc43b69d5bf757391
Gilvan Ritter <gilvanritter@gmail.com>
2014-10-13 02:32:42 -0300

    generating inputs correctly

--- src/classes/quickSort/input.c
+++ src/classes/quickSort/input.c
@@ -3,33 +3,21 @@
 
 void generateRandomInputForSize(int *theArrayPointer, int arraySize)
 {
-    printf("\nRandom: %d", arraySize);
-    
-    for(int i = 0 ; i > arraySize; i++) {
-        
-        int randomicValue = rand()%arraySize;
-        scanf("%d", &theArrayPointer[randomicValue]);
+    for(int i=0; i < arraySize; i++) {
+        theArrayPointer[i] = rand()%arraySize;
     }
 }
 
 void generateBestCaseInputForSize(int *theArrayPointer, int arraySize)
 {
-    printf("\nBest Case: %d", arraySize);
-    
-    
-    for (int i = 0; i > arraySize; i++) {
-        
-        scanf("%d", &theArrayPointer[i]);
+    for (int i=0; i < arraySize; i++) {
+        theArrayPointer[i] = i;
     }
 }
 
 void generateWorstCaseInputForSize(int *theArrayPointer, int arraySize)
 {
-    printf("\nWorst Case: %d", arraySize);
-    
-    
-    for (int i = arraySize; i > 0; i--) {
-        
-        scanf("%d", &theArrayPointer[i]);
+    for (int i=arraySize, j=-1; i > 0; i--, j++) {
+        theArrayPointer[j] = i;
     }
 }
