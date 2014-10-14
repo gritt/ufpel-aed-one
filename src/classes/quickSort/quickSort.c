6d34df8669ffa6067988c74d2dbbf15f2d6a2b5c
Gilvan Ritter <gilvanritter@gmail.com>
2014-10-14 00:34:25 -0300

    added quickSort and partition function

--- src/classes/quickSort/quickSort.c
+++ src/classes/quickSort/quickSort.c
@@ -1,1 +1,50 @@
 #include <stdio.h>
+
+#include "quickSort.h"
+#include "array.h"
+
+void quickSort(int *theArray, int pivot, int right) {
+    
+    int aux;
+    
+    printTheArray(theArray, right);
+    
+    if (pivot < right) {
+        
+        aux = partition(theArray, pivot, right);
+        
+        quickSort(theArray, pivot, aux - 1);
+        quickSort(theArray, aux + 1, right);
+    }
+}
+
+
+int partition(int *theArray, int l, int r)
+{
+    int pivot, i, j, t;
+    
+    pivot = theArray[l];
+    
+    i = l; j = r+1;
+    
+    while(1) {
+        
+        do ++i; while( theArray[i] <= pivot && i <= r );
+        do --j; while( theArray[j] > pivot );
+        
+        if ( i >= j ){
+            break;
+        }
+        
+        t = theArray[i];
+        theArray[i] = theArray[j];
+        theArray[j] = t;
+        
+    }
+    
+    t = theArray[l];
+    theArray[l] = theArray[j];
+    theArray[j] = t;
+    
+    return j;
+}
