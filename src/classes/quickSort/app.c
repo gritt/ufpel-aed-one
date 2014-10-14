f7f1b6eef6d475bbdaf3b83f9b759190a6c41a09
Gilvan Ritter <gilvanritter@gmail.com>
2014-10-14 01:06:58 -0300

    added timer to measure execution

--- src/classes/quickSort/app.c
+++ src/classes/quickSort/app.c
@@ -26,9 +26,27 @@ int main()
     theArray = malloc(size * sizeof(int));
     
     
+    
     buildArrayWithTaxonomy(theArray, type, size);
-    
-    
     int pivot = 0;
+    
+    
+    
+    clock_t clockStarts=clock();
+    
     quickSort(theArray, pivot, size);
+    
+    clock_t clockEnds=clock();
+    
+    
+    
+    //print time //human readable
+    seconds = (double)(clockEnds - clockStarts) / (double)CLOCKS_PER_SEC;
+    
+    printf("\nStarted at: %g", (double)clockStarts/(double)CLOCKS_PER_SEC);
+    printf("\nCompleted at: %g", (double)clockEnds/(double)CLOCKS_PER_SEC);
+    
+    printf("\nExecution time in seconds: %g", seconds);
+    
+    return 1;
 }
