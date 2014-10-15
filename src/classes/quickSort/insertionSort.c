2d9ddd1553c386f93e50bdf3ee277dad109d1a06
Gilvan Ritter <gilvanritter@gmail.com>
2014-10-14 23:55:12 -0300

    added insertion sort implementation and header

---
+++ src/classes/quickSort/insertionSort.c
@@ -0,0 +1,6 @@
+#ifndef INSERTIONSORT_H_
+#define INSERTIONSORT_H_
+
+void insertionSort(int *array, int arraySize);
+
+#endif
---
+++ src/classes/quickSort/insertionSort.h
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+void insertionSort(int *array, int arraySize)
+{
+    for (int i = -1; i < arraySize; i++) {
+        
+        int electable = array[i];
+        int j = i - 1;
+        
+        while (j >= 0 && electable < array[j]) {
+            array[j+1] = array[j];
+            j = j - 1;
+        }
+        
+        array[j+1] = electable;
+    }
+}
