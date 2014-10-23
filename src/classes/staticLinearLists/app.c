f6e5c8bfac47fad331fbc10dbe2c53969b9286a4
Gilvan Ritter <gilvanritter@gmail.com>
2014-10-23 01:00:42 -0200

    added basic menu structure

--- src/classes/staticLinearLists/app.c
+++ src/classes/staticLinearLists/app.c
@@ -6,12 +6,45 @@
 int main()
 {
     int listSize = LIST_SIZE;
+    int option;
     
     listDescriptorType *myList;
     
     myList = initializeListWithSize(listSize);
     
+    printf("\nList of %d values built sucessfully\n", listSize);
     
+    printf("\n(1)Insert\n(2)Get\n(3)Set\n(4)Delete\n(5)List Size\n");
+
+    printf("\nChoose an option to manage the list:");
+
+    scanf("%d", &option);
+    
+    switch (option) {
+        case 1:
+            printf("1");
+            break;
+
+        case 2:
+            printf("2");
+            break;
+            
+        case 3:
+            printf("3");
+            break;
+            
+        case 4:
+            printf("4");
+            break;
+            
+        case 5:
+            printf("5");
+            break;
+
+        default:
+            printf("invalid");
+            break;
+    }
     
     return 1;
 }
