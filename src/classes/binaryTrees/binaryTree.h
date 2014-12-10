5bfd6673b077f5258463523e80e3f2a8a36b8c84
Gilvan Ritter <gilvanritter@gmail.com>
2014-12-10 00:34:16 -0200

    added structs

--- src/classes/binaryTrees/binaryTree.h
+++ src/classes/binaryTrees/binaryTree.h
@@ -6,4 +6,16 @@
 #include <stdbool.h>
 #include <string.h>
 
+struct treeDescriptor {
+    int size;
+    int *rootNode;
+}
+
+struct node {
+    int key;
+    int *father;
+    int *leftNode;
+    int *rightNode;
+}
+
 #endif
